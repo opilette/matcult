5392c76a74fd64350abb7a452a4501825c2a4103
Olivier Pilette <pilette.o@gmail.com>
2023-03-06 22:57:00 -0500

    affmat function update to make it more neutral

--- R/affmat.R
+++ R/affmat.R
@@ -1,9 +1,11 @@
 affmat <- function(x) {
-  
+
   lapply(c("tidyverse"), require, character.only = TRUE)
   x <- x %>% pivot_wider(names_from = c(2), values_from = c(3))
   x[is.na(x)] <- 0
-  x <- tibble::column_to_rownames(x, "site")
+  names(x)[1] <- ""
+  x <- x %>% as.data.frame()
+  x <- data.frame(x[,-1], row.names=x[,1])
   bin <- x %>% mutate_if(is.numeric, ~1 * (. > 0))
   assign("affMatTot", x, envir = .GlobalEnv)
   assign("affMatBin", bin, envir = .GlobalEnv)
